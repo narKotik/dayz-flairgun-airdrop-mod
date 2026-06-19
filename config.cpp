8de060456728c0e7718f5f7dc062ed32673346a0
Oleksandr Simia <oleksandr.simia@gmaail.com>
2026-06-19 19:09:18 +0300

    fix: add DZ_Weapons_Pistols to requiredAddons so FlareGun script class is available

--- config.cpp
+++ config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
         units[] = {};
         weapons[] = {};
         requiredVersion = 0.1;
-        requiredAddons[] = {"DZ_Weapons_Projectiles", "DZ_Weapons_Firearms", "DZ_Data"};
+        requiredAddons[] = {"DZ_Weapons_Projectiles", "DZ_Weapons_Firearms", "DZ_Weapons_Pistols", "DZ_Data"};
     };
 };
 
